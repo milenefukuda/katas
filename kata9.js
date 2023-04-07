30eec9e63b685f9f98a4f43d24de003c33a5b324
Milene Fukuda <milene.fukuda@gmail.com>
2023-04-07 11:21:07 +0100

    daily

---
+++ kata9.js
@@ -0,0 +1,7 @@
+// retornar a maior string contendo letras únicas de dois parametros S1 e S2
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+
+function longest(s1, s2) {
+  let result = [];
+  let newStrin = s1.concat(s2);
+}
